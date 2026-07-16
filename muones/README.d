d05c394bd3d5c7c0ec6012557893e322be2dc82f
Erick Velázquez <erick.velaz.c@gmail.com>
2026-07-16 12:37:49 -0600

    Create README.d

---
+++ muones/README.d
@@ -0,0 +1,11 @@
+# Estimación de la masa de una partícula que decae en dos muones
+
+Se obtienen las masas de distintas partículas, que se desintegran en muones, al leer y analizar datos del LHC (Large Hedron Collider) sobre sus energías y momentos medidos. Se determina, además, qué particulas se descompusieron.
+
+  ## Archivos
+- `muones_instrucciones.pdf` — enunciado del problema
+- `MuRun2010B.csv` — datos experimentales
+  `Jpsimumu_Run2011A.csv` — datos experimentales
+  `Partículas.py` — análisis de datos experimentales
+  `BosonZ.py` — análisis de datos experimentales
+- `resultados_muones.pdf` — desarrollo analítico y resultados
